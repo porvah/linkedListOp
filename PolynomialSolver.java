e41819a1f28294a27719c40410ee63182280cabe
pervmind <3msabdullah@gmail.com>
2023-03-17 23:59:08 +0200

    set poly() done

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -339,13 +339,16 @@ public class PolynomialSolver implements IPolynomialSolver{
             throw new Exception();
         }
     }
-//comment 3beet
+
     @Override
     public void setPolynomial(char poly, int[][] terms) throws Exception {
+        ILinkedList list = new DoubleLinkedList();
         try{
-            ILinkedList list = listFinder(poly);
+            list = listFinder(poly);
         }catch(Exception e){throw new Exception();}
-
+        for(int i = 0; i < terms.length; i++){
+            list.add(terms[i][0]);
+        }
     }
 
     @Override
